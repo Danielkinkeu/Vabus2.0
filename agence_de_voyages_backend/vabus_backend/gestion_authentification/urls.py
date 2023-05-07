19f0bbf24568f931f378918365d03270933058d6
Danielkinkeu <kinkeufranck@gmail.com>
2023-05-07 15:41:31 +0100

    reset pwd

--- agence_de_voyages_backend/vabus_backend/gestion_authentification/urls.py
+++ agence_de_voyages_backend/vabus_backend/gestion_authentification/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from gestion_authentification.views import *
 from gestion_authentification import views
 
 urlpatterns = [
     path('login',views.LoginView.as_view(), name='login'),   
-    path('register',views.RegistrationView.as_view(), name='register'),      
-    path('password_reset/', PasswordResetAPIView.as_view()),
-    path('password_reset_confirm/<uidb64>/<token>/', PasswordResetConfirmAPIView.as_view()),    
+    path('register',views.RegistrationView.as_view(), name='register'), 
+    path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),     
+    # path('password_reset/', PasswordResetAPIView.as_view()),
+    # path('password_reset_confirm/<uidb64>/<token>/', PasswordResetConfirmAPIView.as_view()),    
 ]
--- agence_de_voyages_backend/vabus_backend/vabus_backend/settings.py
+++ agence_de_voyages_backend/vabus_backend/vabus_backend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'gestion_reservation',
     'corsheaders',
     'rest_framework',
+    'django_rest_passwordreset',
     'gestion_user.apps.GestionUserConfig',
     'gestion_admin.apps.GestionAdminConfig',
     'rest_framework_simplejwt',
